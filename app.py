252de69e042d018bff40a2c79926d27a0ea9aeee
Sanjay Nair <nair.sanjay1103@gmail.com>
2021-12-06 22:25:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,11 +67,11 @@ def pneumoniapredictPage():
             if 'image' in request.files:
                 img = np.asarray(Image.open(request.files['image']).convert('L').resize((36, 36))).reshape(
                     (1, 36, 36, 1)) / 255.0
-                pred = np.argmax(load_model("models/pneumonia.h5").predict(img)[0])
+                pred2 = np.argmax(load_model("models/pneumonia.h5").predict(img)[0])
         except:
             message = "Please upload an Image"
             return render_template('pneumonia.html', message = message)
-    return render_template('pneumonia_predict.html', pred = pred)
+    return render_template('pneumonia_predict.html', pred = pred2)
 
 
 if __name__ == '__main__':
