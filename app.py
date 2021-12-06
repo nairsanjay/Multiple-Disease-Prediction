1ef607079afc4a3bf31aa1c06e012b328a174602
Sanjay Nair <nair.sanjay1103@gmail.com>
2021-12-06 22:08:47 +0530

    add pneumonia prediction to web

--- app.py
+++ app.py
@@ -60,5 +60,18 @@ def predictPage():
     return render_template('predict.html', pred = pred)
 
 
+@app.route("/pneumoniapredict", methods = ['POST', 'GET'])
+def pneumoniapredictPage():
+    if request.method == 'POST':
+        try:
+            if 'image' in request.files:
+                pred = np.argmax(load_model("models/pneumonia.h5").predict(
+                    np.asarray(Image.open(request.files['image']).convert('L').resize((36, 36))).reshape(
+                        (1, 36, 36, 1)) / 255.0)[0])
+        except:
+            return render_template('pneumonia.html', message="No image found!")
+    return render_template('pneumonia_predict.html', pred = pred)
+
+
 if __name__ == '__main__':
 	app.run(debug = True)
