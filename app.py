ae759757d3c444139a6a4bbfd12298e9f3d1720a
Sanjay Nair <nair.sanjay1103@gmail.com>
2021-12-06 22:27:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,10 +64,9 @@ def predictPage():
 def pneumoniapredictPage():
     if request.method == 'POST':
         try:
-            if 'image' in request.files:
-                img = np.asarray(Image.open(request.files['image']).convert('L').resize((36, 36))).reshape(
-                    (1, 36, 36, 1)) / 255.0
-                pred2 = np.argmax(load_model("models/pneumonia.h5").predict(img)[0])
+            img = np.asarray(Image.open(request.files['image']).convert('L').resize((36, 36))).reshape(
+                (1, 36, 36, 1)) / 255.0
+            pred2 = np.argmax(load_model("models/pneumonia.h5").predict(img)[0])
         except:
             message = "Please upload an Image"
             return render_template('pneumonia.html', message = message)
